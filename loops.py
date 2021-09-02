91e12b048dc2150b0998f29dc0202ba5282bf210
andressaucedo <andressaucedo83@gmail.com>
2021-09-02 10:45:00 -0700

    Added comments and changed variable nanmes in loopy.py

--- loops.py
+++ loops.py
@@ -1,12 +1,14 @@
 # loops.py - This program is a simple illustration of the syntax used in python
-# for if-else statements, for loops and lists.
+# for if-else statements, for loops and sets.
 def loopy(friend):
-  list = {
+  set_folks = {
   "John","Jane","Diane","Bobby","Lottie","Jimmy"
-  }
+  } 
+# Curly brackets make this a set. Unlike lists or tuples, sets are 
+# unordered. This means the set output order will vary.
 
   print("I'm looking for my friend, what's your name?")
-  for name in list:
+  for name in set_folks:
     print(name)
     if name == friend:
       print(f"Hey, {name}! Good to see ya!" + "\n")
