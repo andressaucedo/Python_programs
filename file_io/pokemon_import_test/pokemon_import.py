35258624fbb8297f22630a92ca2ee96913ad37d3
andressaucedo <andressaucedo83@gmail.com>
2021-10-08 09:40:40 -0700

    added comments to pokemon_import.py

--- file_io/pokemon_import_test/pokemon_import.py
+++ file_io/pokemon_import_test/pokemon_import.py
@@ -1,6 +1,10 @@
+# pokemon_import.py - test program to import a master list and create a party
+# which gets exported to a json file. 
+
 def pokemon_importer(file_path):
     import json
 
+    # Declaring the Pokemon object class and attributes
     class Pokemon:
         def __init__(self, name, types, hp, attacks, weaknesses):
             self.name = name
@@ -9,22 +13,26 @@ def pokemon_importer(file_path):
             self.attacks = attacks
             self.weaknesses = weaknesses    
 
+    # Opening data file in the function call argument
     datafile = open(file_path,)
 
     if file_path == None:
         print("Invalid path argument")
         print(f"Usage: {__name__} [ JSON file path]\n")
         return 1
-
+    # Create dict from json file
     master_dict = json.load(datafile)
     while(1):
+        # Prompt the user for a pokemon name.
         print("'quit' to end")
         pokemon_name = input("Enter a pokemon name: ")
         if pokemon_name == 'quit':
             outfile.close()
             break
         found = 0
+        # Search the imported file as dict for the given name of pokemon.
         for i in master_dict:
+            # If found, pokemon assigned to object, found flag set to 1.
             if i['name'].lower() == pokemon_name.lower():
                 found = 1
                 buddy = Pokemon(
@@ -36,6 +44,7 @@ def pokemon_importer(file_path):
         
         if found != 1:
             print("Your entry was not found")
+        # Print out the object attributes from found pokemon 
         else:    
             print (f"\tName: {buddy.name}")
             for i in buddy.types:
@@ -45,6 +54,7 @@ def pokemon_importer(file_path):
                 print (f"\tAttack: {i['name']} - Damage: {i['damage']}")
             print (f"\tWeakness: {buddy.weaknesses[0]['type']} - Multiplier: {buddy.    weaknesses[0]['value']}")
 
+        # Option to add pokemon to output file 'myparty.json'
         choice = input("Add to Party?: ")
         if choice[0].lower() == 'y':
             bd = vars(buddy)
